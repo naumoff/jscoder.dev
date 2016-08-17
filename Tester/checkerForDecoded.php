492421f57fb13728a334011083b8c1d535c51062
naumoff <andrey.naumoff@gmail.com>
2016-08-18 00:33:06 +0300

    user interface created. work almost complete!

---
+++ Tester/checkerForDecoded.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<title>Script Checker</title>
+</head>
+<body>
+<h1>Old coded Script</h1>
+<script src="../Data/JS-coded/js_coded.js"></script>
+<h1>New decoded Script</h1>
+<script src="../Data/JS-decoded/js_decoded.js"></script>
+</body>
+</html>
